f84eeca33394a91ca0e84086beb6318e97ee5fce
sccma <sccma@qq.com>
2021-10-09 14:31:36 +0800

    oner

---
+++ data/logs/20211008.php
@@ -0,0 +1,460 @@
+<?php exit;?>	2021-10-08 10:53:31 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'site/token/index',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'method' => 'application.token',
+    'file' => 'application.build',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '500',
+    'status' => 'Internal Error',
+    'responseline' => 'HTTP/1.1 500 Internal Error',
+    'headers' => 
+    array (
+      'Date' => 'Fri, 08 Oct 2021 02:53:31 GMT',
+      'Content-Type' => 'charset=utf-8',
+      'Set-Cookie' => 'OAUTH_SESSIONID=tjn21bdmchl68744cj4pcpnluk; expires=Fri, 22-Oct-2021 02:53:22 GMT; Max-Age=1209600; path=/; domain=.w7.cc',
+      'Server' => 'swoole-http-server',
+      'Content-Length' => '84',
+      'X-NWS-LOG-UUID' => '12243937477657984500',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => 'a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+    'meta' => 'HTTP/1.1 500 Internal Error
+Date: Fri, 08 Oct 2021 02:53:31 GMT
+Content-Type: charset=utf-8
+Set-Cookie: OAUTH_SESSIONID=tjn21bdmchl68744cj4pcpnluk; expires=Fri, 22-Oct-2021 02:53:22 GMT; Max-Age=1209600; path=/; domain=.w7.cc
+Server: swoole-http-server
+Content-Length: 84
+X-NWS-LOG-UUID: 12243937477657984500
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:31 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => NULL,
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Fri, 08 Oct 2021 02:53:31 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '18443619769735238',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Fri, 08 Oct 2021 02:53:31 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 18443619769735238
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:35 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'site/token/index',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'method' => 'application.token',
+    'file' => 'application.build',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '500',
+    'status' => 'Internal Error',
+    'responseline' => 'HTTP/1.1 500 Internal Error',
+    'headers' => 
+    array (
+      'Date' => 'Fri, 08 Oct 2021 02:53:35 GMT',
+      'Content-Type' => 'charset=utf-8',
+      'Set-Cookie' => 'OAUTH_SESSIONID=671v9qtc4edshujaqb8cccln6i; expires=Fri, 22-Oct-2021 02:53:35 GMT; Max-Age=1209600; path=/; domain=.w7.cc',
+      'Server' => 'swoole-http-server',
+      'Content-Length' => '84',
+      'X-NWS-LOG-UUID' => '14949169173305449921',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => 'a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+    'meta' => 'HTTP/1.1 500 Internal Error
+Date: Fri, 08 Oct 2021 02:53:35 GMT
+Content-Type: charset=utf-8
+Set-Cookie: OAUTH_SESSIONID=671v9qtc4edshujaqb8cccln6i; expires=Fri, 22-Oct-2021 02:53:35 GMT; Max-Age=1209600; path=/; domain=.w7.cc
+Server: swoole-http-server
+Content-Length: 84
+X-NWS-LOG-UUID: 14949169173305449921
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:35 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => NULL,
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Fri, 08 Oct 2021 02:53:35 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '181231506423119302',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Fri, 08 Oct 2021 02:53:35 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 181231506423119302
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:39 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'site/token/index',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'method' => 'application.token',
+    'file' => 'application.build',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '500',
+    'status' => 'Internal Error',
+    'responseline' => 'HTTP/1.1 500 Internal Error',
+    'headers' => 
+    array (
+      'Date' => 'Fri, 08 Oct 2021 02:53:39 GMT',
+      'Content-Type' => 'charset=utf-8',
+      'Set-Cookie' => 'OAUTH_SESSIONID=0du6juvv36tqnh28146kvg5b9a; expires=Fri, 22-Oct-2021 02:53:39 GMT; Max-Age=1209600; path=/; domain=.w7.cc',
+      'Server' => 'swoole-http-server',
+      'Content-Length' => '84',
+      'X-NWS-LOG-UUID' => '18361304048338543251',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => 'a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+    'meta' => 'HTTP/1.1 500 Internal Error
+Date: Fri, 08 Oct 2021 02:53:39 GMT
+Content-Type: charset=utf-8
+Set-Cookie: OAUTH_SESSIONID=0du6juvv36tqnh28146kvg5b9a; expires=Fri, 22-Oct-2021 02:53:39 GMT; Max-Age=1209600; path=/; domain=.w7.cc
+Server: swoole-http-server
+Content-Length: 84
+X-NWS-LOG-UUID: 18361304048338543251
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:39 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => NULL,
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Fri, 08 Oct 2021 02:53:39 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '7601672091655354822',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Fri, 08 Oct 2021 02:53:39 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 7601672091655354822
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:41 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'site/token/index',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'method' => 'application.token',
+    'file' => 'application.build',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '500',
+    'status' => 'Internal Error',
+    'responseline' => 'HTTP/1.1 500 Internal Error',
+    'headers' => 
+    array (
+      'Date' => 'Fri, 08 Oct 2021 02:53:41 GMT',
+      'Content-Type' => 'charset=utf-8',
+      'Set-Cookie' => 'OAUTH_SESSIONID=qu8sqtt29kpl1srh4v3cbsm162; expires=Fri, 22-Oct-2021 02:53:32 GMT; Max-Age=1209600; path=/; domain=.w7.cc',
+      'Server' => 'swoole-http-server',
+      'Content-Length' => '84',
+      'X-NWS-LOG-UUID' => '2980282170708884525',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => 'a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+    'meta' => 'HTTP/1.1 500 Internal Error
+Date: Fri, 08 Oct 2021 02:53:41 GMT
+Content-Type: charset=utf-8
+Set-Cookie: OAUTH_SESSIONID=qu8sqtt29kpl1srh4v3cbsm162; expires=Fri, 22-Oct-2021 02:53:32 GMT; Max-Age=1209600; path=/; domain=.w7.cc
+Server: swoole-http-server
+Content-Length: 84
+X-NWS-LOG-UUID: 2980282170708884525
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+a:3:{s:5:"errno";i:500;s:4:"data";a:0:{}s:7:"message";s:21:"站点信息不完整";}',
+  ),
+)
+<?php exit;?>	2021-10-08 10:53:41 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 0,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => NULL,
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'rJx9',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Fri, 08 Oct 2021 02:53:41 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '12602212207431004577',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Fri, 08 Oct 2021 02:53:41 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 12602212207431004577
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
