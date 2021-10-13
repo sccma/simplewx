d5b6dc057ddda105694839555227d99d8fa746bb
sccma <sccma@qq.com>
2021-10-13 15:38:32 +0800

    ok

--- data/logs/20211009.php
+++ data/logs/20211009.php
@@ -648,3 +648,783 @@ X-Cache-Lookup: Cache Miss
 ',
   ),
 )
+<?php exit;?>	2021-10-09 19:52:27 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1fa352c9a5f806f7498d4b798e65eefd',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'G6S5',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:52:27 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '8706940650529936130',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:52:27 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 8706940650529936130
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:52:30 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1fa352c9a5f806f7498d4b798e65eefd',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'G6S5',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:52:29 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '16447772610585925727',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:52:29 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 16447772610585925727
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:52:31 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1fa352c9a5f806f7498d4b798e65eefd',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'G6S5',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:52:31 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '17503691491373464668',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:52:31 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 17503691491373464668
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:54:02 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1fa352c9a5f806f7498d4b798e65eefd',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'G6S5',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:54:02 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '15611976502859351097',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:54:02 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 15611976502859351097
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:57:23 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1fa352c9a5f806f7498d4b798e65eefd',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'G6S5',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:57:23 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '419110567889534750',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:57:23 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 419110567889534750
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:58:56 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:58:56 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '54354080752184878',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:58:56 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 54354080752184878
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:08 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:08 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '14978699516467970852',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:08 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 14978699516467970852
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:08 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:08 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '4796368940116949988',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:08 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 4796368940116949988
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:15 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:15 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '6686508331790359854',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:15 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 6686508331790359854
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:21 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:21 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '13976520094123483104',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:21 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 13976520094123483104
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:23 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:23 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '989342552753991588',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:23 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 989342552753991588
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
+<?php exit;?>	2021-10-09 19:59:28 cloud-api-error :
+------------
+Array:
+array (
+  'method' => 'we7/site/notify/system',
+  'data' => 
+  array (
+    'host' => 'vx.pplu.site',
+    'https' => 1,
+    'family' => 'v',
+    'version' => '2.7.10',
+    'php_version' => '7.2.33',
+    'current_host' => 'vx.pplu.site',
+    'release' => '202109270001',
+    'key' => 277503,
+    'password' => 'beb8c98845ae048fe681528f96289c0c',
+    'client' => 'cf101a3337d104b790016cfe13a07e96',
+    'token' => '1e539145672e2b4a167ae7dbe514cc3f',
+  ),
+  'extra' => 
+  array (
+    'X-We7-Cache' => 'v56k',
+  ),
+  'response' => 
+  array (
+    'code' => '404',
+    'status' => 'Not Found',
+    'responseline' => 'HTTP/1.1 404 Not Found',
+    'headers' => 
+    array (
+      'Server' => 'ias/1.4.2_1.17.3',
+      'Date' => 'Sat, 09 Oct 2021 11:59:28 GMT',
+      'Content-Type' => 'text/html',
+      'Content-Length' => '157',
+      'X-NWS-LOG-UUID' => '1552327711549821108',
+      'Connection' => 'keep-alive',
+      'X-Cache-Lookup' => 'Cache Miss',
+    ),
+    'content' => '<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+    'meta' => 'HTTP/1.1 404 Not Found
+Server: ias/1.4.2_1.17.3
+Date: Sat, 09 Oct 2021 11:59:28 GMT
+Content-Type: text/html
+Content-Length: 157
+X-NWS-LOG-UUID: 1552327711549821108
+Connection: keep-alive
+X-Cache-Lookup: Cache Miss
+
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>ias/1.4.2_1.17.3</center>
+</body>
+</html>
+',
+  ),
+)
